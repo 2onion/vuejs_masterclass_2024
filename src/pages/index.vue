64fbf6ce6caa0ae58f1e66033768dd6ea9a20d88
denis <denisxd4@gmail.com>
2025-11-09 12:39:36 +0100

    changed the nome of the views file to make them visible in the routing, and use them as routes for the application

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,7 +4,7 @@
 
 <template>
   <h1>Home page</h1>
-  <RouterLink to="/projects">Go to Projects</RouterLink>
+  <RouterLink :to="{name: '/projects/[id]', params: {id: 1}}">Go to Project 1</RouterLink>
 </template>
 
 <style scoped>
