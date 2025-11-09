a669247e47ebf43fa3d7c7646da2287ebeb9ea12
denis <denisxd4@gmail.com>
2025-11-09 11:03:35 +0100

    created the basic routing for the home page and the Project page using the dynamic routing, created the relatives views for Home and Projects and inserted the main and the call for the RouterView in the App file

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>Home page</h1>
+  <RouterLink to="/projects">Go to Projects</RouterLink>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>Projects Page</h1>
+  <RouterLink to="/">Go to Home</RouterLink>
+</template>
+
+<style scoped>
+
+</style>
